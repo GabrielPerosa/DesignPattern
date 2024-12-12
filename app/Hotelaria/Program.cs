71528f86895fc3f5432e7460f634a963b5684b63
Giovani-RodriguesS <giovanirodrigues021@gmail.com>
2024-12-11 23:04:59 -0300

    update: novareserva has been refactored

--- app/Hotelaria/Program.cs
+++ app/Hotelaria/Program.cs
@@ -8,6 +8,8 @@ using Hotelaria.Models.Quartos;
 using Hotelaria.Models.Adicionais;
 using Hotelaria.Models.Gerente;
 
+// Terminar Adicionais na função NovaReserva
+
 bool ExibirMenu = true;
 int opcao = 0;
 Gerente gerente = Gerente.ObterInstancia();
@@ -26,57 +28,7 @@ while(ExibirMenu)
     switch(opcao)
     {
         case 1:
-            // Nome do cliente
-            Console.Clear();
-            Console.WriteLine("Digite o nome do cliente:");
-            string cliente = Console.ReadLine();
-
-            bool validado = false;
-            // Escolha do quarto
-            while (!validado)
-            {
-                Console.Clear();
-                Console.WriteLine("Escolha um quarto:");
-                Console.WriteLine("1 - Quarto Básico");
-                Console.WriteLine("2 - Quarto Médio");
-                Console.WriteLine("3 - Quarto Luxo");
-                Console.WriteLine("4 - Cancelar\n");
-
-                opcao = int.Parse(Console.ReadLine());
-                
-                switch(opcao)
-                {
-                    case 1:
-                        Console.Clear();
-                        gerente.AdicionarReserva(QuartoFactory.CriarQuarto(TipoQuarto.Economico), cliente);
-                        Thread.Sleep(2000);
-                        validado = true;
-                        break;
-                    case 2:
-                        Console.Clear();
-                        gerente.AdicionarReserva(QuartoFactory.CriarQuarto(TipoQuarto.Medio), cliente);
-                        Thread.Sleep(2000); 
-                        validado = true;
-                        break;
-                    case 3:
-                        Console.Clear();
-                        gerente.AdicionarReserva(QuartoFactory.CriarQuarto(TipoQuarto.Luxo), cliente);
-                        Thread.Sleep(2000); 
-                        validado = true;
-                        break;
-                    case 4:
-                        Cancelado();
-                        validado = true;
-                        break;
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Opção inválida! Tente novamente.");
-                        Thread.Sleep(1000);
-                        Console.Clear();
-                        break;
-                }
-            }
-            
+            NovaReserva(opcao, gerente);
             break;
         case 2:
             listar(gerente);
@@ -182,4 +134,65 @@ static void Cancelado()
     Thread.Sleep(1000);
     Console.ResetColor();
     Console.Clear();
+}
+
+static void NovaReserva(int opcao, Gerente gerente)
+{
+    // Nome do cliente
+    Console.Clear();
+    Console.WriteLine("Digite o nome do cliente:");
+    string cliente = Console.ReadLine();
+    bool validado = false;
+    // Escolha do quarto
+    while (!validado)
+    {
+        Console.Clear();
+        Console.WriteLine("Deseja qual pacote adicional? ");
+        Console.WriteLine("1 - Vista para a Praia");
+        Console.WriteLine("2 - Café da manhã");
+        Console.WriteLine("3 - Não quero nada\n");
+
+
+
+        Console.Clear();
+        Console.WriteLine("Escolha um quarto:");
+        Console.WriteLine("1 - Quarto Básico");
+        Console.WriteLine("2 - Quarto Médio");
+        Console.WriteLine("3 - Quarto Luxo");
+        Console.WriteLine("4 - Cancelar\n");
+        opcao = int.Parse(Console.ReadLine());
+        
+        switch(opcao)
+        {
+            case 1:
+                Console.Clear();
+                gerente.AdicionarReserva(QuartoFactory.CriarQuarto(TipoQuarto.Economico), cliente);
+                Thread.Sleep(2000);
+                validado = true;
+                break;
+            case 2:
+                Console.Clear();
+                gerente.AdicionarReserva(QuartoFactory.CriarQuarto(TipoQuarto.Medio), cliente);
+                Thread.Sleep(2000); 
+                validado = true;
+                break;
+            case 3:
+                Console.Clear();
+                gerente.AdicionarReserva(QuartoFactory.CriarQuarto(TipoQuarto.Luxo), cliente);
+                Thread.Sleep(2000); 
+                validado = true;
+                break;
+            case 4:
+                Cancelado();
+                validado = true;
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine("Opção inválida! Tente novamente.");
+                Thread.Sleep(1000);
+                Console.Clear();
+                break;
+        }
+    }
+    
 }
